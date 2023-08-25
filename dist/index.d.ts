72e1c8182fdac2435f6250f653a76ee4a2bc86bc
Michal Grňo <m93a.cz@gmail.com>
2023-08-25 13:33:23 +0200

    fix: try removing .ts extension from imports to fix SSR

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-export { ErrorCorrectionLevel, generateCode } from './generator.ts';
-export type { GeneratorProps, GeneratorResult, OneToFourty, ZeroToSeven } from './generator.ts';
+export { ErrorCorrectionLevel, generateCode } from './generator';
+export type { GeneratorProps, GeneratorResult, OneToFourty, ZeroToSeven } from './generator';
 export { default as Code } from './Generator.svelte';
 export { default as Scanner, CamPreference, ColorDetectionMode } from './Scanner.svelte';
